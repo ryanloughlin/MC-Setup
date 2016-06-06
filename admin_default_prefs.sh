702d23a079cea21267d766c37c938215b083648a
ryanloughlin <rloughlin@mac.com>
2016-06-06 13:13:31 -0400

    Update admin_default_prefs.sh

--- admin_default_prefs.sh
+++ admin_default_prefs.sh
@@ -12,7 +12,6 @@
 # Shell
 # ==============================================
 
-echo "export PATH=/usr/local/sbin:$PATH" >> ~/.bash_profile
 echo "export CLICOLOR=1" >> ~/.bash.profile
 echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ~/.bash_profile
 echo "export PS1='\[\033[1;35m\]\u\[\033[m\]@\[\033[37m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ '" >> ~/.bash_profile
