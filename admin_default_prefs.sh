a5b455810ab6b8066f90506ff579fce0cafddd5a
ryanloughlin <rloughlin@mac.com>
2016-06-07 14:43:10 -0400

    Update admin_default_prefs.sh

--- admin_default_prefs.sh
+++ admin_default_prefs.sh
@@ -8,6 +8,8 @@
 #
 # ==============================================
 
+sudo -v
+
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
@@ -227,6 +229,7 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 # ==============================================
 
 # Use dockutil to empty and then rebuild the dock
+dockutil
 dockutil --remove all
 killall Dock
 sleep 3
@@ -358,7 +361,7 @@ defaults write com.apple.Safari WebKitInitialTimedLayoutDelay 0.25
 curl -o /tmp/Custom.terminal.gz http://brego/files/Custom.terminal.gz
 gunzip /tmp/Custom.terminal.gz
 
-open "/tmp/Custom.terminal.gz"
+open "/tmp/Custom.terminal"
 sleep 1
 
 defaults write com.apple.terminal "Default Window Settings" -string "Custom"
