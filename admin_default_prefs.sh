d2d75d44689d5f90e8fe62ebd89c43c9711e6cc7
ryanloughlin <rloughlin@mac.com>
2016-06-08 08:06:56 -0400

    Update admin_default_prefs.sh

--- admin_default_prefs.sh
+++ admin_default_prefs.sh
@@ -430,9 +430,8 @@ sudo tccutil.py --insert org.pmbuko.ADPassMon
 # Kill all affected applications
 killall Dock && killall Finder && killall SystemUIServer
 
-# Unload/reload the launchagent in order to run through the script a second time
-launchctl unload /Users/localadmin/Library/LaunchAgents/us.nh.k12.portsmouth.adminprefs.plist
-launchctl load /Users/localadmin/Library/LaunchAgents/us.nh.k12.portsmouth.adminprefs.plist
+#
+~/.admin_default_prefs.sh
 
 # Remove the launch agent so the script doesn't run on subsequent logins
 rm /Users/localadmin/Library/LaunchAgents/us.nh.k12.portsmouth.adminprefs.plist
